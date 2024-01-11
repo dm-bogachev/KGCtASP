50ff590b89a3de669b8d626270c0cd2c3e7a16a7
dm-bogachev <dm.bogachev@yandex.ru>
2024-01-11 17:27:29 +0300

    Update

--- GCodeData.cs
+++ GCodeData.cs
@@ -1,10 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using static System.Windows.Forms.AxHost;
-using System.Xml.Linq;
 
 namespace KGCtASP
 {
